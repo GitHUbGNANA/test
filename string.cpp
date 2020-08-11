48c8937d0cc40f7f37b64e16d58a5ad8b1f68793
gnana <gnanay999@gmail.com>
2020-08-11 13:59:13 +0530

    string functions are added

---
+++ string.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+// added string functions and overloaded operatorsi info
+using namespace std;
+
+static uint32_t s_AllocCount = 0;
+
+
+void* operator new(size_t size)
+{
+
+
+ s_AllocCount++;
+
+std::cout<< "Allocating"<<size<< "bytes\n";
+ return malloc(size);
+
+}
+
+void PrintName(const std::string& name)
+
+{
+
+   std::cout<< name<< std::endl;
+
+
+
+}
+int main(){
+
+
+std::string name = "Yan chernikov";
+
+std::cout <<s_AllocCount<< "allocations" <<std::endl;
+
+PrintName(name);
+std::cin.get();
+
+
+}
